b8bffdc5733899562c78959a529ce7e27ef14c17
telhadosr <87396474+telhadosr@users.noreply.github.com>
2022-04-05 07:33:25 -0300

    Add files via upload

---
+++ Projeto GitHub/code.js
@@ -0,0 +1,158 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["81a4f500-ad11-4009-8f0e-175b165af74b","11d5d9f0-6932-45de-ba25-6f4ac37fe9a9","7d58941f-a7d8-42da-85c6-3efa6131461a","42b7aa29-c5f8-44fe-9d49-8540deca9c13","e92d7a76-cb9d-426f-90f2-1f5356f539aa"],"propsByKey":{"81a4f500-ad11-4009-8f0e-175b165af74b":{"name":"ball","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/81a4f500-ad11-4009-8f0e-175b165af74b.png","frameSize":{"x":20,"y":20},"frameCount":1,"looping":true,"frameDelay":4,"version":"N.u7s1pHQJbBeLOf0G00AfQCMs3BRWNi","loadedFromSource":true,"saved":true,"sourceSize":{"x":20,"y":20},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/81a4f500-ad11-4009-8f0e-175b165af74b.png"},"11d5d9f0-6932-45de-ba25-6f4ac37fe9a9":{"name":"robot","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/11d5d9f0-6932-45de-ba25-6f4ac37fe9a9.png","frameSize":{"x":77,"y":69},"frameCount":1,"looping":true,"frameDelay":4,"version":"TOZduNfxSG5LMTePNzjiUASl6QLWjtOj","loadedFromSource":true,"saved":true,"sourceSize":{"x":77,"y":69},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/11d5d9f0-6932-45de-ba25-6f4ac37fe9a9.png"},"7d58941f-a7d8-42da-85c6-3efa6131461a":{"name":"player","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/7d58941f-a7d8-42da-85c6-3efa6131461a.png","frameSize":{"x":60,"y":91},"frameCount":1,"looping":true,"frameDelay":4,"version":".Q_WtBMvZIQT_vCrUGEsRGGLHyzzv9Cg","loadedFromSource":true,"saved":true,"sourceSize":{"x":60,"y":91},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/7d58941f-a7d8-42da-85c6-3efa6131461a.png"},"42b7aa29-c5f8-44fe-9d49-8540deca9c13":{"name":"player_kick","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/42b7aa29-c5f8-44fe-9d49-8540deca9c13.png","frameSize":{"x":77,"y":77},"frameCount":1,"looping":true,"frameDelay":4,"version":"OjXJgXG4XN6581vrsB.6BLOy__TRlfZo","loadedFromSource":true,"saved":true,"sourceSize":{"x":77,"y":77},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/42b7aa29-c5f8-44fe-9d49-8540deca9c13.png"},"e92d7a76-cb9d-426f-90f2-1f5356f539aa":{"name":"player_fall","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/e92d7a76-cb9d-426f-90f2-1f5356f539aa.png","frameSize":{"x":92,"y":51},"frameCount":1,"looping":true,"frameDelay":4,"version":"y6YVkcT1zLb27ELTusHef._7f_x7nHs6","loadedFromSource":true,"saved":true,"sourceSize":{"x":92,"y":51},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/e92d7a76-cb9d-426f-90f2-1f5356f539aa.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+//crie a bola(ball), a raqueteJogador(playerPaddle) e a raqueteComputador(computerPaddle) como objetos sprites
+var ball = createSprite(200,200,10,10);
+
+var playerPaddle = createSprite(380,200,10,70);
+var computerPaddle = createSprite(10,200,10,70);
+
+//variável para armazenar diferentes estágios do jogo
+var gameState = "serve";
+
+//variáveis para manter o placar
+var compScore = 0;
+var playerScore = 0;
+
+
+function draw() {
+  //limpe a tela
+  background("white");
+  if(ball.isTouching(computerPaddle)|| ball.isTouching(playerPaddle)){
+    playSound("assets/hit.mp3", false);
+  }
+  
+  //posicione o texto informativo ao centro
+  if (gameState === "serve") {
+    text("Pressione Espaço para Lançar",150,180);
+  }
+   
+  //mostrar placares
+  text(compScore, 170,20);
+  text(playerScore, 230,20);
+  
+  //faça a palheta do jogador mover-se de acordo com a posição y do mouse
+  playerPaddle.y = World.mouseY;
+  
+  //IA para a palheta do computador
+  //faça a se mover de acordo com a posição y da bola
+  computerPaddle.y = ball.y;
+  
+  //desenhe uma linha no centro
+  for (var i = 0; i < 400; i=i+20) {
+    line(200,i,200,i+10);
+  }
+  
+  
+ 
+  
+  //crie limites de borda
+  //faça a bola quicar nas bordas superior e inferior
+  createEdgeSprites();
+  
+  ball.bounceOff(topEdge);
+  ball.bounceOff(bottomEdge);
+  ball.bounceOff(playerPaddle);
+  ball.bounceOff(computerPaddle);
+ 
+  
+  //lance a bola quando espaço é pressionado
+  if (keyDown("space") &&  gameState === "serve") {
+    serve();
+    gameState = "play";
+  }
+  
+ 
+  //volte a bola ao centro se ela cruzar a tela
+  if(ball.x > 400 || ball.x <0) {
+    
+    if(ball.x > 400) {
+      compScore = compScore + 1;
+    }
+    
+    if(ball.x < 0) {
+      playerScore = playerScore + 1;
+    }
+    
+    reset();
+    gameState = "serve";
+  }
+  
+  if (playerScore === 5 || compScore === 5){
+    gameState = "over";
+   text("Fim de Jogo!",170,160);
+   text("Pressione 'R' para Recomeçar",150,180);
+  }
+  
+  if (keyDown("r") && gameState === "over") {
+    gameState = "serve";
+    compScore = 0;
+    playerScore = 0;
+  }
+  
+  drawSprites();
+}
+
+function serve() {
+  ball.velocityX = 3;
+  ball.velocityY = 4;
+}
+
+function reset() {
+  ball.x = 200;
+  ball.y = 200;
+  ball.velocityX = 0;
+  ball.velocityY = 0;
+}
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
